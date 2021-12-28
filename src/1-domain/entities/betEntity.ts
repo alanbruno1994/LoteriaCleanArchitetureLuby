7ae876a6eaf7dc4ed99039159ff9f1018757eb98
alanbruno1994 <bruno47rios@outlook.com>
2021-12-28 09:30:38 -0300

    refactor: modify the file betEntity

--- src/1-domain/entities/betEntity.ts
+++ src/1-domain/entities/betEntity.ts
@@ -2,12 +2,12 @@ import { ITimestamps } from '@domain/timestamps'
 import { IGameEntity } from './gameEntity'
 import { IUserEntity } from './userEntity'
 
-export interface IGameEntityRelations { // Aqui serve para definir as relacoes da entidade
+export interface IBetEntityRelations { // Aqui serve para definir as relacoes da entidade
   game: IGameEntity
   user: IUserEntity
 }
 
-export interface IBetEntity extends ITimestamps, Partial<IGameEntityRelations>{
+export interface IBetEntity extends ITimestamps, Partial<IBetEntityRelations>{
   id: number
   secureId: string
   userId: number
