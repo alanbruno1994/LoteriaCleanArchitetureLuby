1a690076d0c90e42a288dd7ef02709938ca8df41
alanbruno1994 <bruno47rios@outlook.com>
2021-12-30 15:39:10 -0300

    test: add fake elements for use in use cases of access profile

--- tests/mock/fakes/entities/fakeAccessProfileEntity.ts
+++ tests/mock/fakes/entities/fakeAccessProfileEntity.ts
@@ -1,5 +1,50 @@
-import { InputAccessProfileEntity } from '@domain/entities/accessProfileEntity'
+import { IInputCreateAccessProfileDto } from '@business/dto/access/create'
+import { IAccessProfileEntity, InputAccessProfileEntity } from '@domain/entities/accessProfileEntity'
 
 export const fakeCreatedAccessProfileEntity: InputAccessProfileEntity = {
   level: 'admin'
 }
+
+export const fakeNewAccessProfile: IInputCreateAccessProfileDto = {
+  level: 'admin'
+}
+
+export const fakeAccessProfileEntity: IAccessProfileEntity = {
+  ...fakeCreatedAccessProfileEntity,
+  id: 1,
+  secureId: '7b1f3001-6a4b-4bdd-90e9-8a280fff017d',
+  user: undefined,
+  created_at: new Date(),
+  updated_at: new Date()
+}
+
+export const fakeAccessProfileList: IAccessProfileEntity[] = [
+  {
+    id: 1,
+    level: 'admin',
+    secureId: '123e4567-e89b-12d3-a456-426614174000',
+    created_at: new Date('2021-09-21T10:00:00.000Z'),
+    updated_at: new Date('2021-09-21T10:00:00.000Z')
+  },
+  {
+    id: 2,
+    level: 'client',
+    secureId: '123e4567-e89b-12d3-a456-426614174001',
+    created_at: new Date('2021-09-21T10:00:00.000Z'),
+    updated_at: new Date('2021-09-21T10:00:00.000Z')
+  },
+  {
+    id: 3,
+    level: 'manager',
+    secureId: '123e4567-e89b-12d3-a456-426614174002',
+    created_at: new Date('2021-09-21T10:00:00.000Z'),
+    updated_at: new Date('2021-09-21T10:00:00.000Z')
+  },
+  {
+    id: 4,
+    level: 'employee',
+    secureId: '123e4567-e89b-12d3-a456-426614174003',
+    created_at: new Date('2021-09-21T10:00:00.000Z'),
+    updated_at: new Date('2021-09-21T10:00:00.000Z')
+  }
+]
---
+++ tests/mock/fakes/repositories/fakeAccessRepository.ts
@@ -0,0 +1,64 @@
+/* eslint-disable no-void */
+/* eslint-disable @typescript-eslint/no-invalid-void-type */
+import { injectable } from 'inversify'
+import { IAccessProfileEntity, InputAccessProfileEntity } from '@domain/entities/accessProfileEntity'
+import { AccessProfileEntityKeys, IAccessProfileRepository, IInputDeleteAccess, IInputUpdateAccess } from '@business/repositories/accessprofile/iAccessProfileRepository'
+
+// Aqui nos temos classes com ados marretados
+@injectable()
+export class FakeAccessProfileRepository implements IAccessProfileRepository {
+  async findAll (): Promise<void | IAccessProfileEntity[]> {
+    return []
+  }
+
+  // Por padrao defini que vai ser uma situacao de fracasso
+  async delete (_input: IInputDeleteAccess): Promise<IAccessProfileEntity | void> {
+    return void 0
+  }
+
+  async create (i: InputAccessProfileEntity): Promise<IAccessProfileEntity> {
+    return {
+      ...i,
+      id: 1,
+      secureId: 'uuid-uuid-1234-uuid',
+      created_at: new Date(),
+      updated_at: new Date()
+    }
+  }
+
+  async findBy (
+    _t: AccessProfileEntityKeys,
+    _v: IAccessProfileEntity[AccessProfileEntityKeys]
+  ): Promise<IAccessProfileEntity | void> {
+    return void 0
+  }
+
+  async update (_input: IInputUpdateAccess): Promise<IAccessProfileEntity | void> {
+    return void 0
+  }
+}
+// esses elementos estando sendo espiados e poderao ser mocados em testes
+export const fakeAccessProfileRepositoryFindBy = jest.spyOn(
+  FakeAccessProfileRepository.prototype,
+  'findBy'
+)
+
+export const fakeAccessProfileRepositoryCreate = jest.spyOn(
+  FakeAccessProfileRepository.prototype,
+  'create'
+)
+
+export const fakeAccessProfileRepositoryUpdate = jest.spyOn(
+  FakeAccessProfileRepository.prototype,
+  'update'
+)
+
+export const fakeAccessProfileRepositoryDelete = jest.spyOn(
+  FakeAccessProfileRepository.prototype,
+  'delete'
+)
+
+export const fakeAccessProfileRepositoryFindAll = jest.spyOn(
+  FakeAccessProfileRepository.prototype,
+  'findAll'
+)
