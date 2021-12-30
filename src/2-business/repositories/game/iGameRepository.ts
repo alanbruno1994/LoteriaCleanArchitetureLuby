91e720b7e24c57e6fe28a16701d1934dbe1da9d3
alanbruno1994 <bruno47rios@outlook.com>
2021-12-30 14:57:42 -0300

    feat: add reflect-metdata in iGameRepository

--- src/2-business/repositories/game/iGameRepository.ts
+++ src/2-business/repositories/game/iGameRepository.ts
@@ -2,6 +2,7 @@
 import { IGameEntity } from '@domain/entities/gameEntity'
 import { IRelation } from '../relation'
 import { IWhere } from '../where'
+import 'reflect-metadata'
 // Isso aqui serve para dizer que tipos de elementos
 // serao usado como uma chave para um consulta de dados
 // por exemplo
