e20e4126ebe5c51fbff0ac775eaeb39436a07aca
alanbruno1994 <bruno47rios@outlook.com>
2021-12-30 15:00:52 -0300

    test: add fakeUniqueIdentifierSerivce for use in secureId

---
+++ tests/mock/fakes/services/fakeUniqueIdentifierService.ts
@@ -0,0 +1,9 @@
+import { IUniqueIdentifierService } from '@root/src/2-business/services/uniqueIdentifier/iUniqueIdentifier'
+import { injectable } from 'inversify'
+
+@injectable()
+export class FakeUniqueIdentifierService implements IUniqueIdentifierService {
+  create (): string {
+    return 'uuid-1234-rfc-4122'
+  }
+}
