72aea14c05aa207da95f92906d225362d0e88f92
alanbruno1994 <bruno47rios@outlook.com>
2022-01-03 10:36:02 -0300

    refactor: modify findGameByUseCase

--- src/2-business/useCases/game/findGameByUseCase.ts
+++ src/2-business/useCases/game/findGameByUseCase.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable no-void */
 import { IInputFindGameByDto, IOutputFindGameByDto } from '@business/dto/game/findBy'
 import { GameErrors } from '@business/modules/errors/game/gameErrors'
@@ -16,7 +17,7 @@ implements IAbstractUseCase<IInputFindGameByDto, IOutputFindGameByDto> {
   async exec (input: IInputFindGameByDto): Promise<IOutputFindGameByDto> {
     const game = await this.gameRepository.findBy(input.key, input.value)
 
-    if (game == null) {
+    if (!game) {
       return left(GameErrors.gameNotFound())
     }
 
