fe55e6270d986cb19c51b02d2b2999f5f9304aa3
alanbruno1994 <bruno47rios@outlook.com>
2022-01-03 10:39:39 -0300

    feat: add abstractSerializer

---
+++ src/3-controller/serializers/abstractSerializer.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-throw-literal */
+import { validateSync as exec } from 'class-validator'
+
+export abstract class AbstractSerializer<I> {
+  constructor (value: Partial<I>) {
+    Object.assign(this, value)
+  }
+
+  validate (): void {
+    const errors = exec(this)
+    if (errors.length > 0) {
+      throw errors
+    }
+  }
+}
