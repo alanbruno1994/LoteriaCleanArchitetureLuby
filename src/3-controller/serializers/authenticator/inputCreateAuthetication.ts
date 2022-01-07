fb14a4197c3d05a63ea75a9358a3eaf7061abad8
alanbruno1994 <bruno47rios@outlook.com>
2022-01-07 15:02:22 -0300

    feat: add IInputCreateAuthenticationDto

---
+++ src/3-controller/serializers/authenticator/inputCreateAuthetication.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsString, MinLength } from 'class-validator'
+import { AbstractSerializer } from '../abstractSerializer'
+
+interface IInputCreateAuthenticationDto {
+  email: string
+  password: string
+}
+
+export class InputCreateAuthentication extends AbstractSerializer<IInputCreateAuthenticationDto> {
+  @IsString()
+  @IsEmail()
+  email!: string
+
+  @IsString()
+  @MinLength(8)
+  password!: string
+}
