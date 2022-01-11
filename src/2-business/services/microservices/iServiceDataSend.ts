9717610da0fc3369e55fbe411ff828f4380ddc64
alanbruno1994 <bruno47rios@outlook.com>
2022-01-11 14:53:23 -0300

    refactor: altered items in iServiceDataSend

--- src/2-business/services/microservices/iServiceDataSend.ts
+++ src/2-business/services/microservices/iServiceDataSend.ts
@@ -1,10 +1,10 @@
 // Isso aqui vai ser usada para que algem injete dinamente
 // uma depencia em tempo de execucao
 export const IServiceDataSendToken = Symbol.for(
-  'IServiceDataSendeToken'
+  'IServiceDataSendToken'
 )
 // Isso aqui sera usado para que algem no futuro implemente
 // e possa gerar um secure_id, por exemplo
-export interface iServiceDataSend<T> {
+export interface IServiceDataSend<T> {
   sendData: (data: T) => Promise<void>
 }
