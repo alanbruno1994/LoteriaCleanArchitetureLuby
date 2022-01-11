405e446993339421f12f304d8c3a6610e7d798a1
alanbruno1994 <bruno47rios@outlook.com>
2022-01-11 14:55:11 -0300

    feat: add IServiceDataSendToken in DeleteBetUseCase

--- src/2-business/useCases/bet/deleteBetUseCase.ts
+++ src/2-business/useCases/bet/deleteBetUseCase.ts
@@ -1,6 +1,8 @@
 import { IOutputDeleteBetDto } from '@business/dto/bet/delete'
 import { BetErrors } from '@business/modules/errors/bet/betErrors'
 import { IBetRepository, IBetRepositoryToken, IInputDeleteBet } from '@business/repositories/bet/iBetRepository'
+import { IServiceDataSend, IServiceDataSendToken } from '@business/services/microservices/iServiceDataSend'
+import { IBetEntity } from '@domain/entities/betEntity'
 import { left, right } from '@shared/either'
 import { inject, injectable } from 'inversify'
 import { IAbstractUseCase } from '../abstractUseCase'
@@ -9,7 +11,8 @@ import { IAbstractUseCase } from '../abstractUseCase'
 export class DeleteBetUseCase
 implements IAbstractUseCase<IInputDeleteBet, IOutputDeleteBetDto> {
   constructor (
-    @inject(IBetRepositoryToken) private readonly userRepository: IBetRepository
+    @inject(IBetRepositoryToken) private readonly userRepository: IBetRepository,
+    @inject(IServiceDataSendToken) private readonly serviceDataSendRepository: IServiceDataSend<IBetEntity>
   ) {}
 
   async exec (input: IInputDeleteBet): Promise<IOutputDeleteBetDto> {
@@ -20,7 +23,7 @@ implements IAbstractUseCase<IInputDeleteBet, IOutputDeleteBetDto> {
     if (!betToDelete) {
       return left(BetErrors.betFailedToDelete())
     }
-
+    await this.serviceDataSendRepository.sendData(betToDelete)
     return right(betToDelete)
   }
 }
